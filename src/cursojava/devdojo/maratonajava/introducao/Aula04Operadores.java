a87bc1990805d879cc2dabf3322ab08b6271c4a6
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-18 20:42:55 -0300

    upgrade aula04 foi adicionado resto da divisão e operadores lógicos

--- src/cursojava/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/cursojava/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -2,6 +2,7 @@ package cursojava.devdojo.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+        // Operadores (+ - * /)
         int numero1 = 20;
         int numero2 = 20;
         int soma = numero1 + numero2;
@@ -10,6 +11,28 @@ public class Aula04Operadores {
         double divisao = numero1 /(double) numero2;
         String  resultado = "O total da soma é = "+ soma + "; O valor da subtração é = "+subtracao+"; O valor da multiplicação é ="+multiplicacao+"; O valor da divisão é = "+divisao;
         System.out.println(resultado);
+        // para resto utiliza %
+        int resto = 21 % 10;
+        System.out.println("O resto da divisao 21/10 é = "+ resto);
+        /*
+         os operadores lógicos vao retonar valores booleanos (verdeiro ou falso) os valores booleanos são:
+         menor <, maior >, menor igual <=; maior igual >=; igual ==; diferente !=
+         */
+        boolean IsDezMaiorQueVinte = 10 > 20;
+        boolean IsDezMenorQueVinte = 10< 20;
+        boolean IsDezMaiorIgualVinte = 10 >= 20;
+        boolean IsDezMenorIgualVinte = 10 <= 20;
+        boolean IsDezDiferenteDeVinte = 10 != 20;
+
+        System.out.println("É dez maior que vinte "+IsDezMaiorQueVinte);
+        System.out.println("É dez menor que vinte "+ IsDezMenorQueVinte);
+        System.out.println("É dez maior igual a  vinte "+ IsDezMaiorIgualVinte);
+        System.out.println("É dez menor igual a Vinte " + IsDezMenorIgualVinte);
+        System.out.println("É dez diferente de vinte " + IsDezDiferenteDeVinte);
+
+
+
+
 
 
 
