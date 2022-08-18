f4fd070bcdaff3a955cc75fb685a5446e8423cf8
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-18 15:47:43 -0300

    Aula02TiposPrimitivos, criando variaveis

---
+++ src/cursojava/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package cursojava.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        // int, double, char, byte, shor, long, boolean
+        int age = 10;
+        System.out.println("A idade é "+age+" Anos");
+
+    }
+}
