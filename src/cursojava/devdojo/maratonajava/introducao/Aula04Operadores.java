db8da5ac94d3c30b83d420ff3788ac05d1c651de
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-18 20:06:12 -0300

    Aula04Operadores

---
+++ src/cursojava/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,18 @@
+package cursojava.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero1 = 20;
+        int numero2 = 20;
+        int soma = numero1 + numero2;
+        int subtracao = (numero1 - numero2);
+        int multiplicacao = numero1 * numero2;
+        double divisao = numero1 /(double) numero2;
+        String  resultado = "O total da soma é = "+ soma + "; O valor da subtração é = "+subtracao+"; O valor da multiplicação é ="+multiplicacao+"; O valor da divisão é = "+divisao;
+        System.out.println(resultado);
+
+
+
+
+    }
+}
