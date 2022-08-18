111358612355c3f73d296593494d315c2870f351
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-18 17:23:18 -0300

    upgrade aula002TiposPrimitivos

--- src/cursojava/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/cursojava/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,16 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args){
         // int, double, char, byte, shor, long, boolean
         int age = 10;
+        long NumeroGrande = 100000;
+        double SalarioDouble = 2000;
+        float SalarioFloat = 2500;
+        byte IdadeByte = 10;
+        short IdadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+
         System.out.println("A idade é "+age+" Anos");
 
     }
