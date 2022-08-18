47c1d27df9771f166e2aca240859d67cddd58d01
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-18 15:14:03 -0300

    criei comentarios; comentarios de uma lnha, comentarios de mutiplas linha e comentario java doc

--- src/cursojava/devdojo/maratonajava/introducao/OlaDevX.java
+++ src/cursojava/devdojo/maratonajava/introducao/OlaDevX.java
@@ -1,6 +1,15 @@
 package cursojava.devdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentario <b>javadoc</b>
+ */
 public class OlaDevX {
+    // isso é um comentario de uma Linha
+    /* isso é um comentario de
+    mutiplas Linhas
+     */
+    /*
+     */
     public static void main(String[] args){
         System.out.println("KA-ME-HA-ME-HAA");
     }
