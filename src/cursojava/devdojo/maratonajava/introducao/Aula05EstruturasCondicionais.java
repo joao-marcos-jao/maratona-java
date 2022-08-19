8cdf19ef5f470aa9c01c9ce612a5147e16cb4ecd
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-19 14:35:55 -0300

    Aula05EstruturasCondicionais condição if

---
+++ src/cursojava/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,17 @@
+package cursojava.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args){
+        int age = 15;
+        boolean IsAutorizadoComprBebida = age >= 18;
+        //!
+        if (age >=18){
+            System.out.println("Autorizado comprar bebida alcólica");
+        }
+        if (!IsAutorizadoComprBebida ){
+            System.out.println("Não Autorizado comprar bebida alcólica");
+        }
+        System.out.println("Fora do if");
+
+    }
+}
