d557606315bfc378fbeabf329ee5a6b2920667e6
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-19 13:50:49 -0300

    aula04 uprgarde operador de atribuição e contador

--- src/cursojava/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/cursojava/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -1,5 +1,8 @@
 package cursojava.devdojo.maratonajava.introducao;
 
+import java.awt.*;
+import java.sql.SQLOutput;
+
 public class Aula04Operadores {
     public static void main(String[] args) {
         // Operadores (+ - * /)
@@ -29,6 +32,48 @@ public class Aula04Operadores {
         System.out.println("É dez maior igual a  vinte "+ IsDezMaiorIgualVinte);
         System.out.println("É dez menor igual a Vinte " + IsDezMenorIgualVinte);
         System.out.println("É dez diferente de vinte " + IsDezDiferenteDeVinte);
+        /*
+         outros operadores lógicos
+         && (And) que é [e]; || (OR) que é [OU]; ! (Negação)
+         */
+        int age = 29;
+        float salary = 1000F;
+        boolean IsDentroDaLeiMaiorQueTrinta = age > 30 && salary >= 4612;
+        boolean IsDentroDaLeiMenorQueTrinta = age < 30 && salary >= 3381;
+
+        System.out.println("IsDentroDaLeiMaiorQueTrinta"+IsDentroDaLeiMaiorQueTrinta);
+        System.out.println("IsDentroDaLeiMenorQueTrinta"+IsDentroDaLeiMenorQueTrinta);
+
+        double ValorTotalCorrente = 200;
+        double ValorTotalPoupanca = 10000;
+        float ValorPlaystation = 500F;
+        boolean ValorPlaytationCincoCompravel = ValorTotalCorrente > ValorPlaystation || ValorTotalPoupanca > ValorPlaystation;
+        System.out.println("ValorPlaytationCincoCompravel"+ValorPlaytationCincoCompravel);
+
+        // Operadores de Atribuição =, +=, -=, *=, /=, %=
+         double bonus= 1800;
+        bonus = bonus + 1000;
+        bonus = bonus - 1000;
+        bonus = bonus * 2;
+        bonus = bonus % 2;
+        System.out.println("O bonus é igua a "+bonus);
+
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador ++;
+        contador --;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+
+        System.out.println(++contador2);
+
+
+
+
+
+
+
 
 
 
