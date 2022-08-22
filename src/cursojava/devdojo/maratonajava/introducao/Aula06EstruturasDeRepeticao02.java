e0537b46952b9e8ade3fc012f38df6e34b0e20ce
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-22 12:22:15 -0300

    Aula06EstruturasDeRepeticao02 contagem de numeros pares

---
+++ src/cursojava/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,13 @@
+package cursojava.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        // Imprima todos os numero pares de 0 a 1000000
+        for (int i=0; i <= 1000000; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
