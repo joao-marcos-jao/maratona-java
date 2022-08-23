b656db68d48236e7e74d9aff4688d373aba33944
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-23 16:29:05 -0300

    Orientação objeto exercicos classes

---
+++ src/cursojava/devdojo/maratonajava/javacore/introducoesclasses/dominio/Carro.java
@@ -0,0 +1,7 @@
+package cursojava.devdojo.maratonajava.javacore.introducoesclasses.dominio;
+
+public class Carro {
+    public String name;
+    public String Model;
+    public int year;
+}
---
+++ src/cursojava/devdojo/maratonajava/javacore/introducoesclasses/test/carro01.java
@@ -0,0 +1,30 @@
+package cursojava.devdojo.maratonajava.javacore.introducoesclasses.test;
+
+import cursojava.devdojo.maratonajava.javacore.introducoesclasses.dominio.Carro;
+
+import java.awt.*;
+
+public class carro01 {
+    public static void main(String[] args){
+        Carro carro1  = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.name = "Fusca Bala";
+        carro1.Model = "Sport";
+        carro1.year = 1969;
+
+        carro2.name = "Mustang";
+        carro2.Model = "Gt 500";
+        carro2.year = 1968;
+
+        System.out.println(" Carro1");
+        System.out.println(carro1.name);
+        System.out.println(carro1.Model);
+        System.out.println(carro1.year);
+
+        System.out.println(" \n Carro2");
+        System.out.println(carro2.name);
+        System.out.println(carro2.Model);
+        System.out.println(carro2.year);
+    }
+}
