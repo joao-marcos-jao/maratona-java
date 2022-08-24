0ac4f98e9543014bd8b5f1550f707920d0f9b1a6
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-24 17:01:07 -0300

    oientação objeto metododos passa gem de paramentro tipo utilizando arrays

--- src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Cauculadora.java
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Cauculadora.java
@@ -41,5 +41,15 @@ public class Cauculadora {
         System.out.println("num1 "+num1);
         System.out.println("num2 "+num2);
     }
+    public void somaArray (int[] numeros){
+        int soma = 0;
+        for (int num:numeros) {
+            soma += num;
+
+        }
+        System.out.println(soma);
+
+    }
+
 
 }
---
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/test/CauculadoraTest05.java
@@ -0,0 +1,11 @@
+package cursojava.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import cursojava.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Cauculadora;
+
+public class CauculadoraTest05 {
+    public static void main(String[] args){
+        Cauculadora cauculadora = new Cauculadora();
+        int[] numeros= {1,2,3,4,5};
+        cauculadora.somaArray(numeros);
+    }
+}
