2ff477139490c7e526cae029bbc7b953616eacd7
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-24 12:18:57 -0300

    orientação objeto metodo retorno utilizando diferente diferente (!=)

--- src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Cauculadora.java
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Cauculadora.java
@@ -21,11 +21,11 @@ public class Cauculadora {
     }
 
     public double divideDoisNumeros2(double num1, double num2) {
-        if (num2 == 0) {
-            return 0;
-        } else {
+        if (num2 != 0) {
             return num1 / num2;
         }
+        return 0;
+
     }
 
 }
