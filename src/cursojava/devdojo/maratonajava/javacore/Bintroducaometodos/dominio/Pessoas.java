95370afb43adb5fb450a5d5b497622a4f7e122f3
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-24 19:04:06 -0300

    oientação objeto modificadores de acesso, ate entao estamos usando o publico mas tambem existe o privado que iremos utilizar no proximo commit

---
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Pessoas.java
@@ -0,0 +1,11 @@
+package cursojava.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Pessoas {
+    public String nome;
+    public int idade;
+
+    public void imprime () {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
+}
---
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,14 @@
+package cursojava.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import cursojava.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Pessoas;
+
+public class PessoaTest01 {
+    public static void main(String[] args){
+        Pessoas pessoas = new Pessoas();
+        pessoas.nome = "Jiraya";
+        pessoas.idade = 70;
+        pessoas.imprime();
+
+    }
+
+}
