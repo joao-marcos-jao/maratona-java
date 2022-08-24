69db3c70ed3c98add24878a5f69d864bc50d7e2d
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-24 17:32:40 -0300

    oientação objeto metododos passa gem de paramentro tipo utilizando VarArgs

--- src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Cauculadora.java
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Cauculadora.java
@@ -50,6 +50,15 @@ public class Cauculadora {
         System.out.println(soma);
 
     }
+    public void somaVarArgs(int... numeros){
+        int soma = 0;
+        for (int num:numeros) {
+            soma += num;
+
+        }
+        System.out.println(soma);
+
+    }
 
 
 }
--- src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/test/CauculadoraTest05.java
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/test/CauculadoraTest05.java
@@ -7,5 +7,6 @@ public class CauculadoraTest05 {
         Cauculadora cauculadora = new Cauculadora();
         int[] numeros= {1,2,3,4,5};
         cauculadora.somaArray(numeros);
+        cauculadora.somaVarArgs(1,2,3,4,5,6);
     }
 }
