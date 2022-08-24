87b459a8ab79c515a15b0f9239bb93284814f4df
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-08-24 15:29:28 -0300

    oientação objeto metododos paramentro tipo variavel referencia

---
+++ src/cursojava/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoaraEstudantes.java
@@ -0,0 +1,14 @@
+package cursojava.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class ImpressoaraEstudantes {
+    public void imprime(Estudantes estudantes){
+        System.out.println("-------------------");
+
+        System.out.println(estudantes.name);
+        System.out.println(estudantes.age);
+        System.out.println(estudantes.sex);
+
+    }
+
+
+}
