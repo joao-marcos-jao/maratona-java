d19be8092bbd368984d29a061d66a41dd7ecd44f
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-09-05 13:56:54 -0300

    orientaçao obtejto Modificador Final referencia

--- src/cursojava/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/cursojava/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,11 +1,15 @@
 package cursojava.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
 
 import cursojava.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import cursojava.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
 
     }
 }
