7839e1719ab91c665a8d72b55c777b9224697c34
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-09-05 12:32:12 -0300

    orientaçao obtejto Sobreescrita iniciando parte 1

---
+++ src/cursojava/devdojo/maratonajava/javacore/Isobreescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package cursojava.devdojo.maratonajava.javacore.Isobreescrita.test;
+
+import cursojava.devdojo.maratonajava.javacore.Isobreescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+
+    }
+}
