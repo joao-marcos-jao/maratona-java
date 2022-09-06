3204456fbae84282dcc9d07537d5866419e8ee6c
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-09-06 15:25:33 -0300

    ORIENTAÇOA OBJETO POLIMORFISMO : paramentro polimorficos

--- src/cursojava/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/cursojava/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -2,6 +2,7 @@ package cursojava.devdojo.maratonajava.javacore.Npolimorfismo.test;
 
 
 import cursojava.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import cursojava.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import cursojava.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import cursojava.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -9,8 +10,12 @@ public class ProdutoTest01 {
     public static void main (String[] args){
         Computador computador =new Computador("Nuci7", 11000);
         Tomate tomate = new Tomate("Tomate Cereja", 10);
-       CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao televisao = new Televisao("Samsung 50\"", 5000);
+
+       CalculadoraImposto.cacularImposto(computador);
         System.out.println("------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.cacularImposto(tomate);
+        System.out.println("-------------------------");
+        CalculadoraImposto.cacularImposto(televisao);
     }
 }
