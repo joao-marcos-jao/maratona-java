b1a65e258b479b6d9de2fedc64eef37b3ddb1eff
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-09-14 14:25:34 -0300

    Classes Utilitaria Internacionlização de moedas com Locale

--- src/cursojava/devdojo/maratonajava/javacore/Sformatação/test/NumberTest02.java
+++ src/cursojava/devdojo/maratonajava/javacore/Sformatação/test/NumberTest02.java
@@ -4,7 +4,7 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberTest01 {
+public class NumberTest02 {
     public static void main(String[] args) {
         Locale locaDefault = Locale.getDefault();
         Locale localeBR = new Locale("pt", "BR");
@@ -12,17 +12,17 @@ public class NumberTest01 {
         Locale localeIT = Locale.ITALY;
         Locale localeUS = Locale.US;
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance(localeUS);
-        nfa[1] = NumberFormat.getInstance(localeBR);
-        nfa[2] = NumberFormat.getInstance(localeJP);
-        nfa[3] = NumberFormat.getInstance(localeIT);
+        nfa[0] = NumberFormat.getCurrencyInstance(localeUS);
+        nfa[1] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
         double valor = 10_000.2130;
         for (NumberFormat numberFormat : nfa) {
-            numberFormat.getMaximumFractionDigits();
+            System.out.println(numberFormat.getMaximumFractionDigits());
             System.out.println(numberFormat.format(valor));
             
         }
-        String valorString = "10000.2130";
+        String valorString = "$10,000.21";
         try {
             System.out.println(nfa[0].parse(valorString));
         } catch (ParseException e) {
