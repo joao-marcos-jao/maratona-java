81c35ff3e401dafc3a583b1d9fe2c79795942433
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-09-14 13:46:04 -0300

    Classes Utilitaria Internacionlização de numeros

--- src/cursojava/devdojo/maratonajava/javacore/Sformatação/test/NumberTest01.java
+++ src/cursojava/devdojo/maratonajava/javacore/Sformatação/test/NumberTest01.java
@@ -1,5 +1,26 @@
 package cursojava.devdojo.maratonajava.javacore.Sformatação.test;
 
-public class NumberTest01 {
+import java.text.NumberFormat;
+import java.util.Locale;
 
+public class NumberTest01 {
+    public static void main(String[] args) {
+        Locale locaDefault = Locale.getDefault();
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+        Locale localeUS = Locale.US;
+        NumberFormat[] nfa = new NumberFormat[4];
+        nfa[0] = NumberFormat.getInstance(localeUS);
+        nfa[1] = NumberFormat.getInstance(localeBR);
+        nfa[2] = NumberFormat.getInstance(localeJP);
+        nfa[3] = NumberFormat.getInstance(localeIT);
+        double valor = 10_000.2130;
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+            
+        }
+
+
+    }
 }
