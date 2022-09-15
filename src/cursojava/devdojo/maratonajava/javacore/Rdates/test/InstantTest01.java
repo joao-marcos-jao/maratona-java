012d942e89f75d66f8327cae764c331e96e10543
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-09-15 12:22:01 -0300

    Classes Utilitaria Instant

---
+++ src/cursojava/devdojo/maratonajava/javacore/Rdates/test/InstantTest01.java
@@ -0,0 +1,22 @@
+package cursojava.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+        System.out.println(now.getEpochSecond());
+        System.out.println(now.getNano());
+        System.out.println(Instant.ofEpochSecond(3));
+        System.out.println(Instant.ofEpochSecond(3));
+        System.out.println(Instant.ofEpochSecond(3,0));
+        System.out.println(Instant.ofEpochSecond(3,1000000000));
+        System.out.println(Instant.ofEpochSecond(3,-1000000000));
+
+
+    }
+}
