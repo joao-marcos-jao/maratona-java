08ec68c32002ca3d6729003552cfa6e6a29c3e10
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-09-16 17:21:06 -0300

    Classes Utilitaria Date Time Formatter

---
+++ src/cursojava/devdojo/maratonajava/javacore/Rdates/test/ZoneTest01.java
@@ -0,0 +1,47 @@
+package cursojava.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.time.*;
+import java.time.chrono.JapaneseDate;
+import java.util.Map;
+
+public class ZoneTest01 {
+    public static void main(String[] args) {
+        Map<String, String> shortIds = ZoneId.SHORT_IDS;
+        System.out.println(shortIds);
+
+        System.out.println(ZoneId.systemDefault());
+
+        ZoneId tokyoZone = ZoneId.of("Asia/Tokyo");
+        System.out.println(tokyoZone);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+        ZonedDateTime zonedDateTime = now.atZone(tokyoZone);
+        System.out.println(zonedDateTime);
+
+        Instant nowInstant = Instant.now();
+        System.out.println(nowInstant);
+        ZonedDateTime zonedDateTime2 = nowInstant.atZone(tokyoZone);
+        System.out.println(zonedDateTime2);
+
+        System.out.println(ZoneOffset.MIN);
+        System.out.println(ZoneOffset.MAX);
+
+        ZoneOffset ofsetManaus = ZoneOffset.of("-04:00");
+        OffsetDateTime offsetDateTime = now.atOffset(ofsetManaus);
+        System.out.println(offsetDateTime);
+        OffsetDateTime offsetDateTime1 = offsetDateTime.of(now, ofsetManaus);
+        System.out.println(offsetDateTime1);
+        OffsetDateTime offsetDateTime2 = nowInstant.atOffset(ofsetManaus);
+        System.out.println(offsetDateTime2);
+
+        JapaneseDate japaneseDate = JapaneseDate.from(LocalDateTime.now());
+        System.out.println(japaneseDate);
+        LocalDate meijiEraLocalDate = LocalDate.of(1900, 02, 01);
+        JapaneseDate meijiEra = JapaneseDate.from(meijiEraLocalDate);
+        System.out.println(meijiEra);
+
+
+    }
+}
---
+++ src/cursojava/devdojo/maratonajava/javacore/Sformatação/test/DateTimeFormatterTest01.java
@@ -0,0 +1,44 @@
+package cursojava.devdojo.maratonajava.javacore.Sformatação.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeFormatterTest01 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        LocalDate parse1 = LocalDate.parse("20220916",DateTimeFormatter.BASIC_ISO_DATE);
+        LocalDate parse2 = LocalDate.parse("2022-09-16+05:00",DateTimeFormatter.ISO_DATE);
+        LocalDate parse3 = LocalDate.parse("2022-09-16", DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(parse1);
+        System.out.println(parse2);
+        System.out.println(parse3);
+
+        LocalDateTime now = LocalDateTime.now();
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+        LocalDateTime parse4 = LocalDateTime.parse("2022-09-16T15:52:32.335179464", DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(parse4);
+        DateTimeFormatter formatterBr = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formartBr = LocalDate.now().format(formatterBr);
+        System.out.println(formartBr);
+        LocalDate parseBr = LocalDate.parse("16/09/2022", formatterBr);
+        System.out.println(parseBr);
+
+        DateTimeFormatter formatterGR = DateTimeFormatter.ofPattern("dd.MMMM.yyyy", Locale.GERMAN);
+        String formatGR = LocalDate.now().format(formatterGR);
+        System.out.println(formatGR);
+        LocalDate parseGR = LocalDate.parse("16.September.2022", formatterGR);
+        System.out.println(parseGR);
+
+
+    }
+}
