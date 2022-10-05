8ab15d4b89744b97778bf7938c77120b343a9b28
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-10-05 14:33:37 -0300

    Lambdasconsumer

---
+++ src/cursojava/devdojo/maratonajava/javacore/ZZClambdas/test/LambdaTest01.java
@@ -0,0 +1,20 @@
+package cursojava.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LambdaTest01 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("Joao","Marcos", "Lufy", "Zoro");
+        List<Integer> intergers = List.of(1,2,3,4,5);
+        forEach(strings,Lambdas consumer s-> System.out.println(s));
+        forEach(intergers, i-> System.out.println(i));
+
+    }
+    private static <T> void forEach(List<T> list, Consumer<T> consumer){
+        for (T e : list) {
+            consumer.accept(e);
+            
+        }
+    }
+}
