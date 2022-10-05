80db9877dc3b409bc69bf2f5d4348f9b9226bc72
João Marcos Rodrigues de Oliveira <80833001+joao-marcos-jao@users.noreply.github.com>
2022-10-05 14:34:22 -0300

    Lambdas consumer correção

--- src/cursojava/devdojo/maratonajava/javacore/ZZClambdas/test/LambdaTest01.java
+++ src/cursojava/devdojo/maratonajava/javacore/ZZClambdas/test/LambdaTest01.java
@@ -7,7 +7,7 @@ public class LambdaTest01 {
     public static void main(String[] args) {
         List<String> strings = List.of("Joao","Marcos", "Lufy", "Zoro");
         List<Integer> intergers = List.of(1,2,3,4,5);
-        forEach(strings,Lambdas consumer s-> System.out.println(s));
+        forEach(strings, s-> System.out.println(s));
         forEach(intergers, i-> System.out.println(i));
 
     }
